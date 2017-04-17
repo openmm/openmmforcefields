2bcb87ed9f680d486773d629f5e562cfb58610b8
jchodera <john.chodera@choderalab.org>
2017-04-17 14:07:14 -0400

    Update recipe

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Build the python package
-$PYTHON setup.py install
+# Install the conversion tools package
+#$PYTHON setup.py install
+
+# Package the CHARMM forcefield
+CHARMM_FFXML_DEST=""$PREFIX/share/openmm/forcefields/charmm"
+mkdir -p $CHARMM_FFXML_DEST
+cp -r charmm/ffxml/*.xml $CHARMM_FFXML_DEST
+
+# Package the AMBER forcefield
