9356e9a178a36d607352e5294fb29c69696ad06a
John Chodera <john.chodera@choderalab.org>
2017-04-23 23:26:33 -0400

    Fix error in build.sh

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -4,7 +4,7 @@
 #$PYTHON setup.py install
 
 # Package the CHARMM forcefield
-CHARMM_FFXML_DEST=""$PREFIX/share/openmm/forcefields/charmm"
+CHARMM_FFXML_DEST="$PREFIX/share/openmm/forcefields/charmm"
 mkdir -p $CHARMM_FFXML_DEST
 cp -r charmm/ffxml/*.xml $CHARMM_FFXML_DEST
 
