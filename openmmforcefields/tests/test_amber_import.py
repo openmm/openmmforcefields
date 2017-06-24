87b2fae7c91fde1baf8af6244f9f401d7aeaa410
John Chodera <john.chodera@choderalab.org>
2017-06-23 23:28:34 -0400

    More nosetest improvements.

--- openmmforcefields/tests/test_amber_import.py
+++ openmmforcefields/tests/test_amber_import.py
@@ -6,6 +6,7 @@ Test AMBER forcefield imports.
 import os
 import glob
 from functools import partial
+from pkg_resources import resource_filename
 
 def check_ffxml_import(filename):
     """
@@ -20,6 +21,22 @@ def check_ffxml_import(filename):
     from simtk.openmm import app
     ff = app.ForceField(filename)
 
+def check_ffxml_parameterize(pdb_filename, ffxml_filename):
+    """
+    Attempt to load OpenMM ffxml forcefield file and parameterize a molecule.
+
+    Parameters
+    ----------
+    pdb_filename : str
+        The PDB filename.
+    ffxml_filename : str
+        The ffxml forcefield filename.
+
+    """
+    from simtk.openmm import app
+    pdbfile = app.PDBFile(pdb_filename)
+    ff = app.ForceField(ffxml_filename)
+
 def test_amber_import_ff94():
     """
     Test import of ff94
@@ -27,6 +44,14 @@ def test_amber_import_ff94():
     """
     from simtk.openmm import app
     ff = app.ForceField('amber/ff94.xml')
+
+def test_amber_parameterize_ff94():
+    """
+    Test parameterizing explicit villin with ff94
+
+    """
+    pdb_filename = resource_filename('simtk.openmm.app', 'data/test.pdb')
+    check_ffxml_parameterize(pdb_filename, 'amber/ff94.xml')
 
 def test_all_amber_imports():
     """
