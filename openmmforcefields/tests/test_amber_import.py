21b00cc7687cb3ad7cd7e9f8c2e745dc7a6675f6
Ivy Zhang <ivy.zhang@wustl.edu>
2021-09-08 09:32:37 -0400

    use protein.ff14SB not ff14SB

--- openmmforcefields/tests/test_amber_import.py
+++ openmmforcefields/tests/test_amber_import.py
@@ -28,9 +28,13 @@ def test_ffxml_import(filename):
         # Must be used with ff99SB.xml
         filenames = ['amber/ff99SB.xml', 'amber/phosaa10.xml']
         ff = app.ForceField(*filenames)
-    elif filename in ['amber/phosaa14SB.xml', 'amber/GLYCAM_06j-1.xml']:
+    elif filename == 'amber/phosaa14SB.xml':
         # Must be used with ff14SB.xml
-        filenames = ['amber/ff14SB.xml', filename]
+        filenames = ['amber/ff14SB.xml', 'amber/phosaa14SB.xml']
+        ff = app.ForceField(*filenames)
+    elif filename == 'amber/GLYCAM_06j-1.xml':
+        # Must be used with protein.ff14SB.xml
+        filenames = ['amber/protein.ff14SB.xml', 'amber/GLYCAM_06j-1.xml']
         ff = app.ForceField(*filenames)
     else:
         ff = app.ForceField(filename)
