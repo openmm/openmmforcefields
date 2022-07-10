openmm/openmmforcefields

commit f3e93f116128ccb645cc627fcaacac1bcac01315

Remove debug prints. (#198)
Co-authored-by: Mike Henry <11765982+mikemhenry@users.noreply.github.com>

openmmforcefields/generators/template_generators.py
@@ -1035,10 +1035,8 @@ class OpenMMSystemMixin(object):
                 torsions[particle_indices].append( (periodicity, phase, k) )
             else:
                 torsions[particle_indices] = [ (periodicity, phase, k) ]
-        print(torsions.keys()) # DEBUG
 
         # Create torsion definitions
-        torsion_types = etree.SubElement(root, "PeriodicTorsionForce", ordering=improper_atom_ordering)
         for particle_indices in torsions.keys():
             params = dict() # build parameter dictionary
             nterms = len(torsions[particle_indices])
@@ -1048,7 +1046,6 @@ class OpenMMSystemMixin(object):
                 params[f'phase{term+1}'] = as_attrib(phase)
                 params[f'k{term+1}'] = as_attrib(k)
             torsion_type = etree.SubElement(torsion_types, torsion_tag(particle_indices), **classes(particle_indices), **params)
-            print(torsion_type) # DEBUG
 
         # TODO: Handle virtual sites
         virtual_sites = [ particle_index for particle_index in range(system.getNumParticles()) if system.isVirtualSite(particle_index) ]

commit 583be46deb953ab17b6ff6daa46d9403f49d3b03

Fix critical bug introduced in PR #198 that meant no torsions were converted into templates

openmmforcefields/generators/template_generators.py
@@ -1042,6 +1042,7 @@ class OpenMMSystemMixin(object):
                 torsions[particle_indices] = [ (periodicity, phase, k) ]
 
         # Create torsion definitions
+        torsion_types = etree.SubElement(root, "PeriodicTorsionForce", ordering='smirnoff')
         for particle_indices in torsions.keys():
             params = dict() # build parameter dictionary
             nterms = len(torsions[particle_indices])
@@ -1050,6 +1051,7 @@ class OpenMMSystemMixin(object):
                 params[f'periodicity{term+1}'] = as_attrib(periodicity)
                 params[f'phase{term+1}'] = as_attrib(phase)
                 params[f'k{term+1}'] = as_attrib(k)
+            torsion_type = etree.SubElement(torsion_types, torsion_tag(particle_indices), **classes(particle_indices), **params)
 
         # TODO: Handle virtual sites
         virtual_sites = [ particle_index for particle_index in range(system.getNumParticles()) if system.isVirtualSite(particle_index) ]

openmmforcefields/tests/test_template_generators.py
@@ -682,7 +682,7 @@ class TestGAFFTemplateGenerator(unittest.TestCase):
                 return 0
 
         RELATIVE_FORCE_DEVIATION_TOLERANCE = 1.0e-5
-        relative_force_deviation = relative_deviation(openmm_forces['total'], smirnoff_forces['total'])
+        relative_force_deviation = relative_deviation(template_forces['total'], reference_forces['total'])
         if relative_force_deviation > RELATIVE_FORCE_DEVIATION_TOLERANCE:
             # Show breakdown by components
             print('Force components:')