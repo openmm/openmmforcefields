3555d1569a898932e9b543a7955c820672c5f4c7
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-07-12 09:53:27 -0500

    Scattered fixes
    * Pin AmberTools
    * Skip non-small molecule force fields at test time
    * Always run tests, even if OpenEye is not licensed

--- openmmforcefields/tests/test_template_generators.py
+++ openmmforcefields/tests/test_template_generators.py
@@ -780,6 +780,11 @@ class TestSMIRNOFFTemplateGenerator(TestGAFFTemplateGenerator):
         for small_molecule_forcefield in SMIRNOFFTemplateGenerator.INSTALLED_FORCEFIELDS:
             if "ff14sb" in small_molecule_forcefield:
                 continue
+            if "tip" in small_molecule_forcefield:
+                continue
+            if "tip" in small_molecule_forcefield:
+                continue
+
             print(f'Testing energies for {small_molecule_forcefield}...')
             # Create a generator that knows about a few molecules
             # TODO: Should the generator also load the appropriate force field files into the ForceField object?
