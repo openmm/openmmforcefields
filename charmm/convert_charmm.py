ac01455cbf0f620845bf8682961e5bc427e9d9c9
Evan Pretti <pretti@stanford.edu>
2025-01-27 08:57:16 -0800

    Increased flexibility of CHARMM conversion
    * Added functionality to split out residues from selected CHARMM
      parameter files
    
    * Allow specifying "fixes" to the generated XML files: can provide XPath
      to find elements, and a tree of XML elements to append as children to
      those found

--- charmm/convert_charmm.py
+++ charmm/convert_charmm.py
@@ -1,12 +1,15 @@
-from parmed.charmm import CharmmParameterSet
 from parmed import openmm
-import glob
-import yaml
-import hashlib
-import os
-import openmm.app as app
+from parmed.charmm import CharmmParameterSet
 import argparse
+import copy
 import csv
+import glob
+import hashlib
+import io
+import openmm.app as app
+import os
+import xml.etree.ElementTree as etree
+import yaml
 
 
 def main():
@@ -104,55 +107,126 @@ def convert_yaml(yaml_filename, ffxml_dir):
                     citation = cite
                     references[-1]["Reference"] = citation
                     references[-1]["forcefield"] = ff
+ 
+        if "Override" in entry:
+            override_level = int(entry["Override"])
+            if verbose:
+                print("Using override level %d..." % override_level)
+        else:
+            override_level = 0
 
-        # generate recommended combination for charmm36
+        if "TestInclude" in entry:
+            ffxml_include = entry["TestInclude"]
+        else:
+            ffxml_include = []
+       
+        # Load the main CHARMM parameter set
         if verbose:
             print(f"Loading CHARMM parameter sets {charmm_files}...")
         params = CharmmParameterSet(*charmm_files)
-
+        
         if len(exclude_residues) > 0:
             if verbose:
                 print(f"Excluding residues: {exclude_residues}")
             for resname in exclude_residues:
                 del params.residues[resname]
 
+        # Load CHARMM parameter sets to determine residues and patches to split
+        # For each entry, store the name of the XML file containing the residues
+        # and patches split out, the residue names to split out, and the patch
+        # names to split out
+        split_data = []
+        if "split" in source_files:
+            for split_spec in source_files["split"]:
+                split_params = CharmmParameterSet(*split_spec["input"])
+                split_fixes = split_spec.get("fixes", [])
+                split_data.append((os.path.join(ffxml_dir, split_spec["output"]), list(split_params.residues.keys()), list(split_params.patches.keys()), split_fixes))
+
+        # Convert everything together to OpenMM so we can figure out which
+        # patches apply to which residues
         if verbose:
-            print("Converting parameters to OpenMM...")
+            print("Determining patch applicability...")
         params_omm = openmm.OpenMMParameterSet.from_parameterset(params, unique_atom_types=True)
+        valid_residues_for_patch, valid_patches_for_residue = params_omm._determine_valid_patch_combinations(params_omm._find_unused_residues())
 
-        # Set override level
-        if "Override" in entry:
-            override_level = int(entry["Override"])
+        # For the main file, remove all residues and patches that are supposed
+        # to go into the split files, convert from a CHARMM to an OpenMM
+        # parameter set, write it out, then read it back in to check for
+        # validity.
+        if verbose:
+            print("Preparing to write main force field file...")
+
+        params_main = copy.deepcopy(params)
+
+        for split_ffxml_filename, split_residue_names, split_patch_names, split_fixes in split_data:
+            for split_residue_name in split_residue_names:
+                if split_residue_name in params_main.residues:
+                    del params_main.residues[split_residue_name]
+            for split_patch_name in split_patch_names:
+                if split_patch_name in params_main.patches:
+                    del params_main.patches[split_patch_name]
+
+        params_main_omm = openmm.OpenMMParameterSet.from_parameterset(params_main, unique_atom_types=True)
+
+        for residue in params_main_omm.residues.values():
+            residue.override_level = override_level
+        for patch in params_main_omm.patches.values():
+            patch.override_level = override_level
+
+        if verbose:
+            print("Writing main force field file...")
+        params_main_omm.write(ffxml_filename, provenance=provenance, charmm_imp=True, separate_ljforce=True)
+        apply_fixes(ffxml_filename, source_files.get("fixes", []))
+
+        if verbose:
+            print("Reading back main force field file...")
+        main_forcefield = app.ForceField(ffxml_filename, *ffxml_include)
+
+        # For the split files, include only those residues and patches that
+        # belong, plus any residues from the main file that have patches in the
+        # split file applying to them.  ParmEd doesn't give us access to control
+        # writing <ApplyToResidue>, unfortunately.
+        for split_ffxml_filename, split_residue_names, split_patch_names, split_fixes in split_data:
             if verbose:
-                print("Setting residues and patches to override level %d..." % override_level)
-            for name, residue in params_omm.residues.items():
-                residue.override_level = override_level
-            for name, patch in params_omm.patches.items():
-                patch.override_level = override_level
+                print("Preparing to write split force field file...")
 
-        if verbose:
-            print("Writing parameter set and compatible patches. This may take several minutes...")
-        params_omm.write(ffxml_filename, provenance=provenance, charmm_imp=True, separate_ljforce=True)
+            write_residue_names = set(split_residue_names) | {residue_name for patch_name in split_patch_names for residue_name in valid_residues_for_patch.get(patch_name, ())}
+            write_patch_names = set(split_patch_names)
 
-        # Try reading the forcefield back in to make sure it is valid
-        if verbose:
-            print("Verifying ffxml file integrity...")
-        if "TestInclude" in entry:
-            ffxml_include = entry["TestInclude"]
-            forcefield = app.ForceField(ffxml_filename, *ffxml_include)
-        else:
-            forcefield = app.ForceField(ffxml_filename)
+            params_split = copy.deepcopy(params)
+            for residue_name in list(params_split.residues.keys()):
+                if residue_name not in write_residue_names:
+                    del params_split.residues[residue_name]
+            for patch_name in list(params_split.patches.keys()):
+                if patch_name not in write_patch_names:
+                    del params_split.patches[patch_name]
+
+            params_split_omm = openmm.OpenMMParameterSet.from_parameterset(params_split, unique_atom_types=True)
+
+            # These should override the residues and patches in the main file
+            for residue in params_split_omm.residues.values():
+                residue.override_level = override_level + 1
+            for patch in params_split_omm.patches.values():
+                patch.override_level = override_level + 1
+
+            if verbose:
+                print("Writing split force field file...")
+            params_split_omm.write(split_ffxml_filename, provenance=provenance, charmm_imp=True, separate_ljforce=True)
+            apply_fixes(split_ffxml_filename, split_fixes)
+
+            if verbose:
+                print("Reading back split force field file...")
+            split_forcefield = app.ForceField(ffxml_filename, split_ffxml_filename, *ffxml_include)
 
         if "Test" in entry:
             for filename in entry["Test"]:
                 if verbose:
                     print(f"Testing with {filename} ...")
                 pdbfile = app.PDBFile(filename)
-                forcefield.createSystem(pdbfile.topology)
+                main_forcefield.createSystem(pdbfile.topology)
 
         if verbose:
-            print("Verified.")
-
+            print("Done.")
 
 class Logger:
     # logs testing energies into csv
@@ -180,6 +254,32 @@ class Logger:
     def log(self, energies):
         self.writer.writerow(energies)
 
+def apply_fixes(xml_filename, fixes):
+    # Allows things to be done to the force field XML file in an automated way.
+    # Right now the only action is "append" to add a new element.  The target
+    # can use XPath notation to specify the location in the tree where the
+    # modification should take place.
+    tree = etree.parse(xml_filename)
+    for fix in fixes:
+        action = fix["action"]
+        for target_element in tree.findall(fix.get("target", ".")):
+            if action == "append":
+                target_element.append(build_xml_element(fix["content"]))
+            else:
+                raise ValueError(f"Unknown action {action!r}")
+
+    # Pretty print the XML file
+    tree = etree.parse(io.StringIO(etree.canonicalize(etree.tostring(tree.getroot(), encoding="unicode"), strip_text=True)))
+    etree.indent(tree)
+    tree.write(xml_filename, encoding="unicode")
+
+def build_xml_element(data):
+    element = etree.Element(data["tag"], data.get("attrib", {}))
+    element.text = data.get("text", None)
+    element.tail = data.get("tail", None)
+    for child in data.get("children", []):
+        element.append(build_xml_element(child))
+    return element
 
 if __name__ == "__main__":
     main()
