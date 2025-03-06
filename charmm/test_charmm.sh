6c42900501b84d341c6b3354ea08cf84c6fa206c
Evan Pretti <pretti@stanford.edu>
2025-03-05 16:59:40 -0800

    Add CI test for CHARMM in Docker

--- charmm/test_charmm.sh
+++ charmm/test_charmm.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-python test_charmm.py tests/*/*.yaml --charmm --openmm-charmm --openmm-ffxml --openmm-ffxml-fix-impropers --perturb-replicates 5 --perturb-seed 291700478 "$@"
+python test_charmm.py tests/*/*.yaml --charmm --openmm-charmm --openmm-ffxml --openmm-ffxml-fix-impropers --perturb-replicates 3 --perturb-seed 291700478 "$@"
