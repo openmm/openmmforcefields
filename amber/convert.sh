016e67b8801f5b861fd7ce63bcc11285f4f38441
Evan Pretti <pretti@stanford.edu>
2025-03-13 16:31:55 -0700

    Put back old GLYCAM since automated conversion is broken

--- amber/convert.sh
+++ amber/convert.sh
@@ -6,4 +6,10 @@ python convert_amber_ions.py "${AMBERHOME}"
 python convert_amber.py --input solvents.yaml --verbose
 python convert_amber.py --input biopolymer.yaml --verbose
 python convert_amber.py --input gaff.yaml --verbose
-python convert_amber.py --input glycam/glycan.yaml --verbose
+
+# GLYCAM automated conversion is broken (incorrect external bonds get
+# generated): see https://github.com/openmm/openmmforcefields/pull/180.
+# If a new version of the force field is released with AmberTools that we want
+# to support, this will have to be corrected manually again, or a way to check
+# it automatically can be devised.
+# python convert_amber.py --input glycam/glycan.yaml --verbose
