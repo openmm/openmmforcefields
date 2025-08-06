3ef326302ae3b7338b75fa594d27f133a6710402
Evan Pretti <pretti@stanford.edu>
2025-08-06 10:17:11 -0700

    Remove references to and tests for DummySystemGenerator

--- openmmforcefields/generators/__init__.py
+++ openmmforcefields/generators/__init__.py
@@ -1,4 +1,4 @@
-from .system_generators import SystemGenerator, DummySystemGenerator
+from .system_generators import SystemGenerator
 from .template_generators import (
     EspalomaTemplateGenerator,
     GAFFTemplateGenerator,
